e3217778b34f4b56b27cae5df658bd4259a44b05
Preetham <preetham@gmail.com>
2024-11-13 10:58:26 +0530

    Quick_sort function and initialization

--- quick_sort.c
+++ quick_sort.c
@@ -32,3 +32,28 @@ int partition(int arr[], int low, int high) {
     return j;
     
 }
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+
+       
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+  
+    int arr[] = { 4, 2, 5, 3, 1 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    
+    quickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
