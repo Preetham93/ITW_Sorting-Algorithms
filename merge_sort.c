029dbb8a835cf9cb9bbff27cd97a23999a6b03a1
Preetham <preetham@gmail.com>
2024-11-13 10:47:35 +0530

    Mergesort function

--- merge_sort.c
+++ merge_sort.c
@@ -46,3 +46,17 @@ void merge(int arr[], int left, int mid, int right) {
         k++;
     }
 }
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+      
+        
+        int mid = left + (right - left) / 2;
+
+        
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        
+        merge(arr, left, mid, right);
+    }
+}
